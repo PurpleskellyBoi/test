9dcfa453e9c418e7641dba1d28b18d6f1c622888
PurpleskellyBoi <168924467+PurpleskellyBoi@users.noreply.github.com>
2024-05-04 12:40:10 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# import csv
+# from teachersort import *
+# from sortstudents import * 
+# from formatstudents import *
+# from emailtoname import *
+# from teacherdetect import * 
+
+def pyMain(filepath):
+  print(filepath " IT WORKS")
+  # Format(sortStudents(getTeacherList(filepath), filepath))
+
+
+
+
+
+
